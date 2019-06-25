60fbb2230c82b47ae56777b29eb14155163e93f3
Monika Filipowska <>
2019-06-25 13:55:14 +0200

    form

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
-console.log('Hi, Hey, Hello');
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+function checkWeather(city) {
+	fetch('http://localhost:3000/weather?address=' + city).then((response) => {
+		response.json().then((data) => {
+			if (data.error) {
+				messageOne.textContent = data.error;
+			} else {
+				messageOne.textContent = data.location;
+				messageTwo.textContent = data.forecast;
+			}
+		});
+	});
+}
+
+weatherForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	messageOne.textContent = 'Loading...';
+	messageTwo.textContent = '';
+	const location = search.value;
+	checkWeather(location);
+});
